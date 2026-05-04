6e7db255eb2fd1426e39dcd10152d81b8f30d8cd
NSoslovskiy <nazarii.soslovskiy@gmail.com>
2026-05-04 17:31:07 +0300

    переробка для ліміту

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,7 +1,11 @@
-a = [76, 73, 69, 63, 67, 24, 42]
+import sys
+
+data = sys.stdin.read()
+a = list(map(int, data.split()))
 
 for i in range(0, len(a)-1):
     m = max(a[i:])
     idx = a.index(m, i)
     a[i], a[idx] = a[idx], a[i]
-    print(a)
+
+sys.stdout.write(str(a) + '\n')
